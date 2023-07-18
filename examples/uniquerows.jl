2f6433289f2cad575c236bd3fcd2d5bebabeae3c
Nikos Pitsianis <pitsianis@yahoo.com>
2023-07-18 15:04:57 -0400

    simple example unique rows

---
+++ examples/uniquerows.jl
@@ -0,0 +1,10 @@
+using AdaptiveHierarchicalRegularBinning
+
+X = rand(1000, 2)
+X = vcat(X,X[1:100:901,:])
+
+@time u = hcat(unique(eachrow(X))...)'
+
+# OK solve this using the tree
+
+@time tree = regural_bin(UInt32, X, 8, 100; dims=2)
