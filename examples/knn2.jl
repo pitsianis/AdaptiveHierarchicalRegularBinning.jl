925160406fff1ed5a35606b8c7d5252b33c5391e
Nikos Pitsianis <pitsianis@yahoo.com>
2023-07-20 12:15:26 -0400

    cleanup

--- examples/knn2.jl
+++ examples/knn2.jl
@@ -3,7 +3,7 @@ using AbstractTrees
 
 # Setup
 n = 100000
-d = 2
+d = 4
 dims = 2
 
 dpt = 4
@@ -17,31 +17,6 @@ end
 
 tree = regular_bin(UInt128, X, dpt, sml; dims=dims)
 
-# Callbacks
-function leaf_cb(leaf)
-  setcontext!(leaf, extrema(points(leaf); dims=leaddim(leaf))[:])
-end
-
-function node_cb(node)
-  r = [ (Inf, -Inf) for _ in 1:bitlen(node) ]
-
-  for child in children(node)
-    ctx = getcontext(child)
-    for (d, (m, M)) in enumerate(ctx)
-      r[d] = (min(r[d][1], m), max(r[d][2], M))
-    end
-  end
-
-  setcontext!(node, r)
-end
-
-# Evaluate
-# Tree context -> Vector of size d, each element is a tuple of (min, max) per dim
-applypostorder!(tree, leaf_cb, node_cb)
-
-map(x -> x[1], getcontext(tree)) .- minimum(points(tree), dims=dims)
-
-## redo with built-in AbstractTrees functions, I am learning!
 function tightbox(node)
   if isleaf(node)
     setcontext!(node, extrema(points(node); dims=leaddim(node))[:])
