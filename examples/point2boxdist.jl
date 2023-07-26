118a1480876265512535285fae011babfd0d073e
Nikos Pitsianis <pitsianis@yahoo.com>
2023-07-26 12:17:27 -0400

    cleanup

--- examples/point2boxdist.jl
+++
@@ -1,8 +0,0 @@
-# I am olny saving this to keep it for further testing in the future
-function point2boxDist(p, node)
-  h = box(node)
-  c = center(node)
-  
-  sqrt(sum(max.(0.0, max(c .- h .- p, p .- (c .+ h))).^2))
-
-end
